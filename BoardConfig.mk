0ba0ef1c43f8ae8057f489090dc115dee65fa210
Semavi Ulusoy <doc.divxm@gmail.com>
2021-09-03 23:21:16 +0300

    op8Pro: commonize kernel defconfig
    Change-Id: I6f0f20835a2b04e8250d26625c697f344048a905

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,9 +31,6 @@ SOONG_CONFIG_ONEPLUS_KONA_FOD_SIZE = 232
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
-# Kernel Dedfconfig
-TARGET_KERNEL_CONFIG := neutrino_defconfig
-
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
