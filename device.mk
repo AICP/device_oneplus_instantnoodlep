e628c4fcf9dab90c941326b6d4b8437be77c36d1
Semavi Ulusoy <doc.divxm@gmail.com>
2022-08-29 23:04:44 +0300

    op8Pro: prepare for A13
    Change-Id: I9e16d363ee473a054539f39022754f3f6ad7411f

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aicp
 
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
